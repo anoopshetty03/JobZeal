6a94e691f9fe903bc6f360984053f12415c5301f
anoopshetty03 <148759181+anoopshetty03@users.noreply.github.com>
2024-06-30 12:05:10 +0530

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "jobzeal",
+      dbName: "jobzealdb",
     })
     .then(() => {
       console.log("Connected to database.");
